0549d72ef2d6a4ef9234a3940ee187eb60308fd2
Albert Shteiner <ckazzku@gmail.com>
2018-01-05 16:31:10 +0500

    Update version 0.11.0 -> 0.11.1

--- src/harbour-zatraty.cpp
+++ src/harbour-zatraty.cpp
@@ -71,7 +71,7 @@ int main(int argc, char *argv[])
 {
     QGuiApplication *app = SailfishApp::application(argc, argv);
     app->setApplicationName("zatraty");
-    app->setApplicationVersion("0.11.0");
+    app->setApplicationVersion("0.11.1");
 
     QString locale = QLocale::system().name();
     qDebug() << "detected locale is " << locale;
