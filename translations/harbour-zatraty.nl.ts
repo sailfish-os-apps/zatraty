f86c421e1ea7937908c70f9dcc2ee4a5cd7b2b1b
d9h20f <>
2018-01-30 13:18:02 +0100

    Dutch translation

---
+++ translations/harbour-zatraty.nl.ts
@@ -0,0 +1,413 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About %1</source>
+        <translation>Over %1</translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation>Versie %1</translation>
+    </message>
+    <message>
+        <source>A simple app to manage your expenses.</source>
+        <translation>Een eenvoudige app om je uitgaven te beheren.</translation>
+    </message>
+    <message>
+        <source>Source code</source>
+        <translation>Broncode</translation>
+    </message>
+</context>
+<context>
+    <name>BackupsPage</name>
+    <message>
+        <source>Create backup</source>
+        <translation>Back-up maken</translation>
+    </message>
+    <message>
+        <source>Data backups</source>
+        <translation>Back-ups van gegevens</translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation>Herstellen</translation>
+    </message>
+    <message>
+        <source>Applying backup</source>
+        <translation>Back-up wordt hersteld</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Verwijderen</translation>
+    </message>
+    <message>
+        <source>Deleting backup</source>
+        <translation>Back-up wordt verwijderd</translation>
+    </message>
+</context>
+<context>
+    <name>CategoriesPage</name>
+    <message>
+        <source>Add category</source>
+        <translation>Categorie toevoegen</translation>
+    </message>
+    <message>
+        <source>Categories</source>
+        <translation>Categorieën</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Hernoemen</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Verwijderen</translation>
+    </message>
+</context>
+<context>
+    <name>CategoryPage</name>
+    <message>
+        <source>For all time</source>
+        <translation>Voor altijd</translation>
+    </message>
+    <message>
+        <source>For current month</source>
+        <translation>Voor deze maand</translation>
+    </message>
+    <message>
+        <source>for this month</source>
+        <translation>voor deze maand</translation>
+    </message>
+    <message>
+        <source>for all time</source>
+        <translation>voor altijd</translation>
+    </message>
+    <message>
+        <source>Expenses:</source>
+        <translation>Uitgaven:</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Verwijderen</translation>
+    </message>
+    <message>
+        <source>of the category total</source>
+        <translation>van het categorietotaal</translation>
+    </message>
+    <message>
+        <source>of the total</source>
+        <translation>van het totaal</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>This month:</source>
+        <translation>Deze maand:</translation>
+    </message>
+    <message>
+        <source>%1 %2</source>
+        <comment>1 is amount and 2 is currency</comment>
+        <translation>%2%1</translation>
+    </message>
+</context>
+<context>
+    <name>CurrenciesPage</name>
+    <message>
+        <source>Currencies</source>
+        <translation>Munteenheden</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Verwijderen</translation>
+    </message>
+    <message>
+        <source>Deleting currency</source>
+        <translation>Munteenheid wordt verwijderd</translation>
+    </message>
+    <message>
+        <source>Add currency</source>
+        <translation>Munteenheid toevoegen</translation>
+    </message>
+</context>
+<context>
+    <name>DataBase</name>
+    <message>
+        <source>Required sqlite driver not available!</source>
+        <translation>Het vereiste sqlite-stuurprogramma is niet beschikbaar!</translation>
+    </message>
+    <message>
+        <source>Food</source>
+        <translation>Eten</translation>
+    </message>
+    <message>
+        <source>Travels</source>
+        <translation>Reizen</translation>
+    </message>
+    <message>
+        <source>Cannot find local storage dir %1!</source>
+        <translation>Kan lokale opslagmap %1 niet vinden!</translation>
+    </message>
+    <message>
+        <source>There is no data base with name %1!</source>
+        <translation>Er is geen database met de naam %1!</translation>
+    </message>
+    <message>
+        <source>Cannot open data base: %1!</source>
+        <translation>Kan database niet openen: %1!</translation>
+    </message>
+</context>
+<context>
+    <name>DeleteCategoryDialog</name>
+    <message>
+        <source>!</source>
+        <comment>The exclamation mark in the DeleteCategory View</comment>
+        <translation>!</translation>
+    </message>
+    <message>
+        <source>Delete category</source>
+        <translation>Categorie verwijderen</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Verwijderen</translation>
+    </message>
+    <message>
+        <source>All your data related with this category (%1) will be lost!</source>
+        <translation>Al je gegevens in deze categorie (%1) zullen verloren gaan!</translation>
+    </message>
+</context>
+<context>
+    <name>DeleteEntryDialog</name>
+    <message>
+        <source>amount: %1 %2</source>
+        <comment>1 is amount and 2 is currency</comment>
+        <translation>bedrag: %2%1</translation>
+    </message>
+    <message>
+        <source>Delete item</source>
+        <translation>Item verwijderen</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Verwijderen</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Total: %1 %2</source>
+        <comment>1 is amount and 2 is currency</comment>
+        <translation>Totaal: %2%1</translation>
+    </message>
+    <message>
+        <source>Most used category: %1</source>
+        <translation>Meest gebruikte categorie: %1</translation>
+    </message>
+    <message>
+        <source>History</source>
+        <translation>Geschiedenis</translation>
+    </message>
+    <message>
+        <source>Categories</source>
+        <translation>Categorieën</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Instellingen</translation>
+    </message>
+    <message>
+        <source>spent this month</source>
+        <comment>subtitle of the amount spent in the MainView</comment>
+        <translation>deze maand uitgegeven</translation>
+    </message>
+    <message>
+        <source>Last month: %1 %2</source>
+        <comment>1 is amount and 2 is currency</comment>
+        <translation>Voorbije maand: %2%1</translation>
+    </message>
+    <message>
+        <source>Add entry</source>
+        <translation>Uitgave toevoegen</translation>
+    </message>
+</context>
+<context>
+    <name>HistoryPage</name>
+    <message>
+        <source>History</source>
+        <translation>Geschiedenis</translation>
+    </message>
+    <message>
+        <source>%1 %2</source>
+        <comment>1 is amount and 2 is currency</comment>
+        <translation>%2%1</translation>
+    </message>
+</context>
+<context>
+    <name>ImportDataDialog</name>
+    <message>
+        <source>Import data</source>
+        <translation>Gegevens importeren</translation>
+    </message>
+    <message>
+        <source>Import</source>
+        <translation>Importeren</translation>
+    </message>
+    <message>
+        <source>Will be imported data from the old Expense app</source>
+        <translation>De gegevens van je oude Expense-app zullen geïmporteerd worden</translation>
+    </message>
+    <message>
+        <source>Data already imported</source>
+        <translation>Gegevens zijn al geïmporteerd</translation>
+    </message>
+</context>
+<context>
+    <name>ImportDataPage</name>
+    <message>
+        <source>Done</source>
+        <translation>Klaar</translation>
+    </message>
+    <message>
+        <source>Import data</source>
+        <translation>Gegevens importeren</translation>
+    </message>
+    <message>
+        <source>Import in progress...</source>
+        <translation>Bezig met importeren…</translation>
+    </message>
+</context>
+<context>
+    <name>MonthSummaryPage</name>
+    <message>
+        <source>%1 %2 in %3</source>
+        <comment>1 is amount, 2 is currency and 3 is the category</comment>
+        <translation>%2%1 in %3</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Verwijderen</translation>
+    </message>
+</context>
+<context>
+    <name>NewCategoryDialog</name>
+    <message>
+        <source>Name</source>
+        <comment>placeholder for category name</comment>
+        <translation>Naam</translation>
+    </message>
+    <message>
+        <source>Category</source>
+        <translation>Categorie</translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation>Bijwerken</translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation>Toevoegen</translation>
+    </message>
+</context>
+<context>
+    <name>NewCurrencyDialog</name>
+    <message>
+        <source>New currancy</source>
+        <translation>Nieuwe munteenheid</translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation>Toevoegen</translation>
+    </message>
+    <message>
+        <source>Currency</source>
+        <comment>placeholder for currency</comment>
+        <translation>Munteenheid</translation>
+    </message>
+</context>
+<context>
+    <name>NewEntryDialog</name>
+    <message>
+        <source>Amount</source>
+        <comment>placeholder for amount</comment>
+        <translation>Bedrag</translation>
+    </message>
+    <message>
+        <source>Desc</source>
+        <comment>placeholder for description</comment>
+        <translation>Beschrijving</translation>
+    </message>
+    <message>
+        <source>New entry</source>
+        <translation>Nieuw item</translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation>Toevoegen</translation>
+    </message>
+    <message>
+        <source>Today</source>
+        <translation>Vandaag</translation>
+    </message>
+</context>
+<context>
+    <name>ResetDatabaseDialog</name>
+    <message>
+        <source>!</source>
+        <comment>The exclamation mark in the DeleteCategory View</comment>
+        <translation>!</translation>
+    </message>
+    <message>
+        <source>Reset database</source>
+        <translation>Database wissen</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Wissen</translation>
+    </message>
+    <message>
+        <source>All your data will be irreversibly lost!</source>
+        <translation>Al je gegevens zullen voorgoed verloren gaan!</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Incorrect index of currency</source>
+        <translation>Onjuiste index van munteenheid</translation>
+    </message>
+    <message>
+        <source>Currency &apos;%s&apos; already exists</source>
+        <translation>Munteenheid &apos;%s&apos; bestaat al</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Currency: </source>
+        <translation>Munteenheid:</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Instellingen</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Over</translation>
+    </message>
+    <message>
+        <source>Reset database</source>
+        <translation>Database wissen</translation>
+    </message>
+    <message>
+        <source>Import data</source>
+        <translation>Gegevens importeren</translation>
+    </message>
+    <message>
+        <source>Backups</source>
+        <translation>Back-ups</translation>
+    </message>
+</context>
+</TS>
